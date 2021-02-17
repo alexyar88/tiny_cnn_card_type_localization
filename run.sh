97004b0897a77f89159bb0d4edb69245527f3f01
alexyar88 <altmer88@mail.ru>
2021-02-17 16:02:50 +0300

    script fix

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ wget \
       sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p'\
   )&id=$drive_id" -O ../data.zip && rm -rf /tmp/cookies.txt
 
-#mkdir -p ../data
-#unzip -qq ../data.zip
+mkdir -p ../data
+unzip -qq ../data.zip
 
 python3 trainer.py
